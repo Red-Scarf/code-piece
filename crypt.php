1c2c68a53b60f21ca0d07c7f89bfe1871c3da47b
red_scard <705762646@qq.com>
2023-02-01 00:18:40 +0800

    固定打乱算法的加密

--- crypt.php
+++ crypt.php
@@ -2,63 +2,37 @@
 
 $GLOBALS['auth_key'] = 'uje8hj98du8jf097tcgy3';
 /**
- * 加密方法
+ * 加密
+ * 先对数据base64加密，然后使用密码本进行异或运算
+ * 利用异或的自反性实现加密解密
+ * 加密后的数据中 前32位为原数据的md5值 用于打乱密码本
  * @param string $string 待加密/解密字符串
  * @param string $operation 加密/解密操作 DECODE=解密
- * @param string $expiry 有效期
  */
-function crypt($string, $operation = 'DECODE', $expiry = 0)
+function specifyCrypt($string, $operation = 'DECODE')
 {
-    // 动态秘钥长度
-    $ckey_length = 4;
-    // 秘钥
-    $key = $GLOBALS['auth_key'];
-    // 秘钥a参与加解密
-    $keya = md5(substr($key, 0, 16));
-    // 秘钥b验证数据完整性
-    $keyb = md5(substr($key, 16, 16));
-    // 秘钥c转化生成的密文
-    $keyc = $ckey_length ? ($operation == 'DECODE' ? substr($string, 0, $ckey_length) : substr(md5(microtime()), -$ckey_length)) : '';
-    // if ($ckey_length) {
-    //     if ($operation == 'DECODE') {
-    //         $keyc = substr($string, 0, $ckey_length);
-    //     } else {
-    //         $keyc = substr(md5(microtime()), -$ckey_length);
-    //     }
-    // } else {
-    //     $keyc = '';
-    // }
+    if ($operation == 'DECODE') {
+        $stringMD5 = substr($string, 0, 32);
+        $string = substr($string, 32);
+        $string = base64_decode($string);
+    } else {
+        $stringMD5 = md5($string);
+    }
+
     // 参与运算的秘钥
-    $cryptkey = $keya . md5($keya . $keyc);
+    $cryptkey = $GLOBALS['auth_key'];
     $key_length = strlen($cryptkey);
-    // 明文 前10位保存时间戳，验证数据有效性 10~26位保存秘钥b
-    // 解密时利用秘钥b验证数据完整性
-    // 解密从 ckey_length 位开始 前 ckey_length 位保存动态秘钥
-    $string = $operation == 'DECODE' ? base64_decode(substr($string, $ckey_length)) : sprintf('%010d', $expiry ? $expiry + time() : 0) . substr(md5($string, $keyb), 0, 16) . $string;
-    // if ($operation == 'DECODE') {
-    //     $string = base64_decode(substr($string, $ckey_length));
-    // } else {
-    //     // 前10位
-    //     $prefix = sprintf('%010d', $expiry ? $expiry + time() : 0);
-    //     // 后续 16 位
-    //     $secondfix = substr(md5($string, $keyb), 0, 16);
-    //     // 最后接上加密数据
-    //     $string = $prefix . $secondfix . $string;
-    // }
     $string_length = strlen($string);
     $result = '';
     $box = range(0, 255);
     $rndkey = [];
-    // 产生密码簿
+    // 产生密码本
     for ($i = 0; $i <= 255; $i++) {
         $rndkey[$i] = ord($cryptkey[$i % $key_length]);
     }
-    // 固定算法打乱密码簿 增加随机性 实际上不会增加密文强度
+    // 固定算法打乱密码本 增加随机性 实际上不会增加密文强度
     for ($i = $j = 0; $i < 256; $i++) {
         $j = ($j + $box[$i] + $rndkey[$i]) % 256;
-        // $temp = $box[$i];
-        // $box[$i] = $box[$j];
-        // $box[$j] = $temp;
         // 交换值
         list($box[$i], $box[$j]) = [$box[$j], $box[$i]];
     }
@@ -67,21 +41,13 @@ function crypt($string, $operation = 'DECODE', $expiry = 0)
         $a = ($a + 1) % 256;
         $j = ($j + $box[$a]) % 256;
         list($box[$a], $box[$j]) = [$box[$j], $box[$a]];
-        // 密码簿得出密钥进行异或 再转成字符串
+        // 密码本得出密钥进行异或 再转成字符串
         $boxTempInd = ($box[$a] + $box[$i]) % 256;
-        $result = chr(ord($string[$i]) ^ ($box[$boxTempInd]));
+        $result .= chr(ord($string[$i]) ^ ($box[$boxTempInd]));
     }
     if ($operation == 'DECODE') {
-        // 没设置过期时间或者还未过期
-        $condition_1 = substr($result, 0, 10) == 0 || substr($result, 0, 10) - time() > 0;
-        // 
-        $condition_2 = substr($result, 10, 16) == substr(md5(substr($result, 26) . $keyb), 0, 16);
-        if ($condition_1 && $condition_2) {
-            return substr($string, 26);
-        } else {
-            return '';
-        }
+        return $result;
     } else {
-        return $keyc . str_replace('=', '', base64_encode($result));
+        return $stringMD5 . str_replace('=', '', base64_encode($result));
     }
 }
